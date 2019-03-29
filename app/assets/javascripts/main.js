814f21c25c5b784cdc90fd3bfdf9bd06b68e59d2
Steve Albonico <steve@albonico.net>
2019-03-29 11:00:17 -0700

    course index teacher and course links build

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,39 +20,36 @@ function allCourses(){
 	$('#courseList').empty()
 	getSession()
 	$.get("/courses.json", function(data) {
-		
 		data.forEach(function(course){
-		$('#courseList').append("<a href='/home' class='course_name'>"+course.name+"</a><p><em>" + course.teacher.name + "</em></p><p class='cdesc'>"+course.description+"</p>")
-	
-		if (!isEnrolled(course.id)){
-			$('#courseList').append("<div id='coursetype"+ course.id +"'> <input type='radio' name='coursetype"+ course.id +"' value='classroom'> Classroom <input checked type='radio' name='coursetype"+ course.id +"' value='online'> Online<br><button id='Enroll"+ course.id +"'>Enroll</button><br><br></div>")
-		} else {$('#courseList').append("<button id='Unenroll"+ course.id +"'>Unenroll</button><br><br>")}
-		
-		$("#Unenroll"+course.id).on('click', function(event){
-		alert(course.id)})
+			$('#courseList').append("<a href='' class = 'course_name' id='course_name"+course.id+"'>"+course.name+"</a><br><a href='' id='teacher_id"+course.id+"'><em>" + course.teacher.name + "</em></a><p class='cdesc'>"+course.description+"</p>")
+				
+			$("#course_name"+course.id).on('click', function(event){
+				event.preventDefault();
+				console.log(course.id)
+			})
 
-		$("#Enroll"+course.id).on('click', function(event){
-		let ctype = $(`input[name="coursetype${course.id}"]:checked`).val()
-		let posting = $.post('/enrollments/new', {"course_type" : ctype, "course_id" : course.id});
-        posting.done(function (data){
-        	
-        	$("#coursetype"+course.id).replaceWith("<button id='Unenroll"+ course.id +"'>Unenroll</button><br><br>")
-        	
-        })
-      
-		})
+			$("#teacher_id"+course.id).on('click', function(event){
+			event.preventDefault();
+			console.log(course.teacher.id)
+			})
 
-		}) 
-		
-		
-		
-		$(".course_name").on('click', function(event){
-		event.preventDefault();
-		alert("yes!")})
+
+			if (!isEnrolled(course.id)){
+				$('#courseList').append("<div id='coursetype"+ course.id +"'> <input type='radio' name='coursetype"+ course.id +"' value='classroom'> Classroom <input checked type='radio' name='coursetype"+ course.id +"' value='online'> Online<br><button id='Enroll"+ course.id +"'>Enroll</button><br><br></div>")}
+			else {$('#courseList').append("<button id='Unenroll"+ course.id +"'>Unenroll</button><br><br>")}
+				
+			$("#Unenroll"+course.id).on('click', function(event){
+				alert(course.id)})
+
+			$("#Enroll"+course.id).on('click', function(event){
+				let ctype = $(`input[name="coursetype${course.id}"]:checked`).val()
+				let posting = $.post('/enrollments/new', {"course_type" : ctype, "course_id" : course.id});
+		        posting.done(function (data){
+		        	$("#coursetype"+course.id).replaceWith("<button id='Unenroll"+ course.id +"'>Unenroll</button><br><br>")
+		      	})
+	    	})
+    	}) 
 	})
-
-
-
 }
 
 function getSession(){
